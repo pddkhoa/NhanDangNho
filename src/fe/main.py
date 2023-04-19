774c0662f7fe0d1a141c9a91d8c41ba96bf7c236
Phan Đại Đăng Khoa <111940212+pddkhoa@users.noreply.github.com>
2023-04-19 15:16:45 +0700

    comit

---
+++ src/fe/main.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from PIL import Image
+from tensorflow.keras.utils import load_img, img_to_array
+import numpy as np
+from keras.models import load_model
+import requests
+from bs4 import BeautifulSoup
+
+model = load_model('models.h5')
+labels = {0: 'champagne', 1: 'cotton candy', 2: 'crimson seedless', 3: 'gewurztraminer', 4: 'glenora', 5: 'kyoho'
+          }
+
+
+
+
+
+def processed_img(img_path):
+    img = load_img(img_path, target_size=(100, 100, 3))
+    img = img_to_array(img)
+    img = img / 255
+    img = np.expand_dims(img, [0])
+    answer = model.predict(img)
+    y_class = answer.argmax(axis=-1)
+    print(y_class)
+    y = " ".join(str(x) for x in y_class)
+    y = int(y)
+    res = labels[y]
+    print(res)
+    return res.capitalize()
+
+
+def run():
+    st.title("Fruits🍍-Vegetable🍅 Classification")
+    img_file = st.file_uploader("Choose an Image", type=["jpg", "png"])
+    if img_file is not None:
+        img = Image.open(img_file).resize((250, 250))
+        st.image(img, use_column_width=False)
+        save_image_path = './upload_images/' + img_file.name
+        with open(save_image_path, "wb") as f:
+            f.write(img_file.getbuffer())
+
+        # if st.button("Predict"):
+        if img_file is not None:
+            result = processed_img(save_image_path)
+            print(result)
+            st.success("**Predicted : " + result + '**')
+            
+        
+
+
+run()
